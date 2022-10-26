99e771277b7c929d3da59d0df45176e4c436d45d
highfreshness <meteo0718@gmail.com>
2022-10-26 16:57:23 +0900

    FBV로 블로그 포스트 상세 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -6,3 +6,7 @@ def index(request):
     posts = Post.objects.all().order_by('-pk')
     # render 요소 3가지 (request, html파일(템플릿 파일), {key:value} )
     return render(request, 'blog/index.html', {'posts':posts,})
+
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/single_post_page.html', {'post':post})
