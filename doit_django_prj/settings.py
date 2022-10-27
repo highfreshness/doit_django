537ea99696481586f1b6a1f1fab6d02d6da047f7
highfreshness <meteo0718@gmail.com>
2022-10-27 13:44:10 +0900

    포스트 목록 페이지에 대표 이미지 출력하기

--- doit_django_prj/settings.py
+++ doit_django_prj/settings.py
@@ -134,6 +134,10 @@ USE_TZ = False
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+# media 설정
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
